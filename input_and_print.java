756dd21862ed5e8221b118fb37e17f8061a94a37
KyleI24 <kyle.intendencia@dcmail.ca>
2024-01-23 18:24:21 -0500

    Update input_and_print.java
    added a new branch "git_class_e"

--- input_and_print.java
+++ input_and_print.java
@@ -5,6 +5,7 @@ class InputPrint {
     { 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
+        //code to ask the durham id number
         String name = scanner.nextLine();
         System.out.println("Hello, World " + name + ". Welcome to Durham College class on OOPS");
         scanner.close();
